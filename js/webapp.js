9ff2da0d64df0e0cab9506d81acf9212400dc914
Nicolas Guilhou <design@dagence.pro>
2011-06-04 09:22:42 +0200

    commit under the sun

--- js/webapp.js
+++ js/webapp.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function(){
+	var closed = true;
 	$(".configure_panel").css('margin-right', '-200px');
 	$(".configure").click(function(){
-		$(".configure_panel").animate({'margin-right': '0'},500);
+		closed =! closed;
+		$(".configure_panel").stop().animate({'margin-right': closed?'-200px':'0'},500);
 	});
 	$(".close_configure").click(function(){
 		$(".configure_panel").animate({'margin-right': '-200px'},500);
